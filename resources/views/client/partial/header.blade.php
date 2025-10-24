682e3e7ab8e79a22aa7daff8139ea2b4a254a24b
AGNAWALE Esdras <agnawaleayantayoesdras@gmail.com>
2025-10-24 12:42:36 +0100

    modification du topbar et changement du titre

--- resources/views/client/partial/header.blade.php
+++ resources/views/client/partial/header.blade.php
@@ -13,7 +13,7 @@
             </div>
         </div>
     </div>
-    <div class="container px-0 ">
+    <div class="container px-0">
         <nav class="navbar navbar-light navbar-expand-xl py-3 ">
             <a href="#hero" class="navbar-brand">
                 <h1 class="text-primary display-6"><span class="text-secondary">E</span><span style="background-color: #ff4880; color: #ffffff">N</span><span class="text-secondary">C</span></h1>
@@ -29,7 +29,7 @@
                     <a href="#programs" class="nav-item nav-link">Formations</a>
                     <a href="#contact" class="nav-item nav-link">Contact</a>
                 </div>
-                <div class="d-flex me-4">
+                <div class="d-flex me-2">
                     <div id="phone-tada" class="d-flex align-items-center justify-content-center">
                         <a href="" class="position-relative wow tada" data-wow-delay=".9s">
                             <i class="fa fa-phone-alt text-primary fa-2x me-4"></i>
@@ -38,12 +38,11 @@
                             </div>
                         </a>
                     </div>
-                    <div class="d-flex flex-column pe-3 border-end border-primary">
-                        <span class="text-primary">Pour toutes questions ?</span>
+                    <div class="d-flex flex-column border-start border-primary ps-3">
+                        <span class="text-primary">Pour toutes questions </span>
                         <a href="tel:+2290164374858"><span class="text-secondary">Tel: +229 0164374858</span></a>
                     </div>
                 </div>
-                <button class="btn-search btn btn-primary btn-md-square rounded-circle" data-bs-toggle="modal" data-bs-target="#searchModal"><i class="fas fa-search text-white"></i></button>
             </div>
         </nav>
     </div>
