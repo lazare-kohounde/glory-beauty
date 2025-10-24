98e1cf66c90d739fb5b9dc948d260c811c074932
AGNAWALE Esdras <agnawaleayantayoesdras@gmail.com>
2025-10-24 11:26:03 +0100

    first commit

--- resources/views/client/partial/footer-sction.blade.php
+++ resources/views/client/partial/footer-sction.blade.php
@@ -1,88 +1,140 @@
 <div class="container-fluid footer py-5 wow fadeIn" data-wow-delay="0.1s">
-        <div class="container py-5">
-            <div class="row g-5">
-                <div class="col-md-6 col-lg-4 col-xl-3">
-                    <div class="footer-item">
-                        <h2 class="fw-bold mb-3"><span class="text-secondary">E</span><span style="background-color: #ff4880; color: #ffffff">N</span><span class="text-secondary">C</span></h2>
-                        <p class="mb-4">La mission principale de l’ENC est de former des professionnels compétents et polyvalents, capables de relever les défis du secteur culinaire et hôtelier...</p>
-                        <div class="border border-primary p-3 rounded bg-light">
-                            <h5 class="mb-3">Newsletter</h5>
-                            <div class="position-relative mx-auto border border-primary rounded" style="max-width: 400px;">
-                                <input class="form-control border-0 w-100 py-3 ps-4 pe-5" type="text" placeholder="Votre email">
-                                <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2 text-white">S'abonner</button>
-                            </div>
+    <div class="container py-5">
+        <div class="row g-5">
+            <div class="col-md-6 col-lg-4 col-xl-3">
+                <div class="footer-item">
+                    <h2 class="fw-bold mb-3"><span class="text-secondary">E</span><span style="background-color: #ff4880; color: #ffffff">N</span><span class="text-secondary">C</span></h2>
+                    <p class="mb-4">La mission principale de l’ENC est de former des professionnels compétents et polyvalents, capables de relever les défis du secteur culinaire et hôtelier...</p>
+                    <div class="border border-primary p-3 rounded bg-light">
+                        <h5 class="mb-3">Newsletter</h5>
+                        <div class="position-relative mx-auto border border-primary rounded" style="max-width: 400px;">
+                            <input class="form-control border-0 w-100 py-3 ps-4 pe-5 fw-bold" type="text" placeholder="Email">
+                            <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2 text-white">S'abonner</button>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-6 col-lg-4 col-xl-3">
-                    <div class="footer-item">
-                        <div class="d-flex flex-column p-4 ps-5 text-dark border border-primary"
-                            style="border-radius: 50% 20% / 10% 40%;">
-                            <p>Lundi: 08h à 18h</p>
-                            <p>Mardi: 08h à 18h</p>
-                            <p>Mercredi: 08h à 18h</p>
-                            <p>Jeudi: 08h à 18h</p>
-                            <p>Vendredi: 08h à 18h</p>
-                            <p>Samedi: Fermé</p>
-                            <p class="mb-0">Dimanche: Fermé</p>
-                        </div>
+            </div>
+            <div class="col-md-6 col-lg-4 col-xl-3">
+                <div class="footer-item">
+                    <div class="d-flex flex-column p-4 ps-5 text-dark border border-primary"
+                        style="border-radius: 50% 20% / 10% 40%;">
+                        <p class="fw-bold">Lundi: 08h à 18h</p>
+                        <p class="fw-bold">Mardi: 08h à 18h</p>
+                        <p class="fw-bold">Mercredi: 08h à 18h</p>
+                        <p class="fw-bold">Jeudi: 08h à 18h</p>
+                        <p class="fw-bold">Vendredi: 08h à 18h</p>
+                        <p class="fw-bold">Samedi: Fermé</p>
+                        <p class="mb-0 fw-bold">Dimanche: Fermé</p>
                     </div>
                 </div>
-                <div class="col-md-6 col-lg-4 col-xl-3">
-                    <div class="footer-item">
-                        <h4 class="text-primary mb-4 border-bottom border-primary border-2 d-inline-block p-2 title-border-radius">LOCALISATION</h4>
-                        <div class="d-flex flex-column align-items-start">
-                            <a href="" class="text-body mb-4"><i class="fa fa-map-marker-alt text-primary me-2"></i> ENC Bénin, Cotonou Agontikon en face de l'université PIGIE.</a>
-                            <a href="" class="text-body mb-4"><i class="fa fa-map-marker-alt text-primary me-2"></i> ENC Bénin, Abomey-Calavi, BJ.</a>
-                            <a href="" class="text-body mb-4"><i class="fa fa-map-marker-alt text-primary me-2"></i> ENC Bénin, Porto-Novo, BJ.</a>
-                            <a href="" class="text-body mb-4"><i class="fa fa-map-marker-alt text-primary me-2"></i> ENC Bénin, Parakou, Borgou BJ.</a>
+            </div>
+            <div class="col-md-6 col-lg-4 col-xl-3">
+                <div class="footer-item">
+                    <h4 class="text-primary mb-4 border-bottom border-primary border-2 d-inline-block p-2 title-border-radius">LOCALISATION</h4>
+                    <div class="d-flex flex-column align-items-start">
+                        <a href="" class="text-body mb-4"><i class="fa fa-map-marker-alt text-primary me-2"></i> ENC Bénin, Cotonou Agontikon en face de l'université PIGIE.</a>
+                        <a href="" class="text-body mb-4"><i class="fa fa-map-marker-alt text-primary me-2"></i> ENC Bénin, Abomey-Calavi, BJ.</a>
+                        <a href="" class="text-body mb-4"><i class="fa fa-map-marker-alt text-primary me-2"></i> ENC Bénin, Porto-Novo, BJ.</a>
+                        <a href="" class="text-body mb-4"><i class="fa fa-map-marker-alt text-primary me-2"></i> ENC Bénin, Parakou, Borgou BJ.</a>
 
-                            <div class="footer-icon d-flex">
-                                <a class="btn btn-primary btn-sm-square me-3 rounded-circle text-white" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-primary btn-sm-square me-3 rounded-circle text-white" href=""><i class="fab fa-twitter"></i></a>
-                                <a href="#" class="btn btn-primary btn-sm-square me-3 rounded-circle text-white"><i class="fab fa-instagram"></i></a>
-                                <a href="#" class="btn btn-primary btn-sm-square rounded-circle text-white"><i class="fab fa-linkedin-in"></i></a>
-                            </div>
+                        <div class="footer-icon d-flex">
+                            <a class="btn btn-primary btn-sm-square me-3 rounded-circle text-white" href=""><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-primary btn-sm-square me-3 rounded-circle text-white" href=""><i class="fab fa-twitter"></i></a>
+                            <a href="#" class="btn btn-primary btn-sm-square me-3 rounded-circle text-white"><i class="fab fa-instagram"></i></a>
+                            <a href="#" class="btn btn-primary btn-sm-square rounded-circle text-white"><i class="fab fa-linkedin-in"></i></a>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-6 col-lg-4 col-xl-3">
-                    <div class="footer-item">
-                        <h4 class="text-primary mb-4 border-bottom border-primary border-2 d-inline-block p-2 title-border-radius">NOTRE GALLERIE</h4>
-                        <div class="row g-3">
-                            <div class="col-4">
-                                <div class="footer-galary-img rounded-circle border border-primary">
-                                    <img src="img/g1.jpg" class="img-fluid rounded-circle p-2" alt="">
-                                </div>
+            </div>
+            <!-- SECTION GALERIE -->
+            <div class="col-md-6 col-lg-4 col-xl-3">
+                <div class="footer-item">
+                    <h4 class="text-primary mb-4 border-bottom border-primary border-2 d-inline-block p-2 title-border-radius">NOTRE GALLERIE</h4>
+                    <div class="row g-3">
+                        <!-- Chaque image déclenche le modal -->
+                        <div class="col-4">
+                            <div class="footer-galary-img rounded-circle border border-primary">
+                                <img src="img/g1.jpg" class="img-fluid rounded-circle p-2 gallery-thumb" data-bs-toggle="modal" data-bs-target="#galleryModal" data-bs-slide-to="0" alt="Image 1">
                             </div>
-                            <div class="col-4">
-                                <div class="footer-galary-img rounded-circle border border-primary">
-                                    <img src="img/g2.jpg" class="img-fluid rounded-circle p-2" alt="">
-                                </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="footer-galary-img rounded-circle border border-primary">
+                                <img src="img/g2.jpg" class="img-fluid rounded-circle p-2 gallery-thumb" data-bs-toggle="modal" data-bs-target="#galleryModal" data-bs-slide-to="1" alt="Image 2">
                             </div>
-                            <div class="col-4">
-                                <div class="footer-galary-img rounded-circle border border-primary">
-                                    <img src="img/g3.jpg" class="img-fluid rounded-circle p-2" alt="">
-                                </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="footer-galary-img rounded-circle border border-primary">
+                                <img src="img/g3.jpg" class="img-fluid rounded-circle p-2 gallery-thumb" data-bs-toggle="modal" data-bs-target="#galleryModal" data-bs-slide-to="2" alt="Image 3">
                             </div>
-                            <div class="col-4">
-                                <div class="footer-galary-img rounded-circle border border-primary">
-                                    <img src="img/g4.jpg" class="img-fluid rounded-circle p-2" alt="">
-                                </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="footer-galary-img rounded-circle border border-primary">
+                                <img src="img/g4.jpg" class="img-fluid rounded-circle p-2 gallery-thumb" data-bs-toggle="modal" data-bs-target="#galleryModal" data-bs-slide-to="3" alt="Image 4">
                             </div>
-                            <div class="col-4">
-                                <div class="footer-galary-img rounded-circle border border-primary">
-                                    <img src="img/g5.jpg" class="img-fluid rounded-circle p-2" alt="">
-                                </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="footer-galary-img rounded-circle border border-primary">
+                                <img src="img/g5.jpg" class="img-fluid rounded-circle p-2 gallery-thumb" data-bs-toggle="modal" data-bs-target="#galleryModal" data-bs-slide-to="4" alt="Image 5">
                             </div>
-                            <div class="col-4">
-                                <div class="footer-galary-img rounded-circle border border-primary">
-                                    <img src="img/g6.jpg" class="img-fluid rounded-circle p-2" alt="">
-                                </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="footer-galary-img rounded-circle border border-primary">
+                                <img src="img/g6.jpg" class="img-fluid rounded-circle p-2 gallery-thumb" data-bs-toggle="modal" data-bs-target="#galleryModal" data-bs-slide-to="5" alt="Image 6">
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <!-- MODAL CAROUSEL -->
+            <div class="modal fade" id="galleryModal" tabindex="-1" aria-labelledby="galleryModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered modal-lg">
+                    <div class="modal-content bg-dark text-white">
+                        <div class="modal-body p-0">
+                            <div id="carouselGallery" class="carousel slide" data-bs-ride="false">
+                                <div class="carousel-inner">
+                                    <!-- Carousel Items -->
+                                    <div class="carousel-item active">
+                                        <img src="img/g1.jpg" class="d-block w-100" alt="Image 1">
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img src="img/g2.jpg" class="d-block w-100" alt="Image 2">
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img src="img/g3.jpg" class="d-block w-100" alt="Image 3">
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img src="img/g4.jpg" class="d-block w-100" alt="Image 4">
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img src="img/g5.jpg" class="d-block w-100" alt="Image 5">
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img src="img/g6.jpg" class="d-block w-100" alt="Image 6">
+                                    </div>
+                                </div>
+                                <!-- Contrôles -->
+                                <button class="carousel-control-prev" type="button" data-bs-target="#carouselGallery" data-bs-slide="prev">
+                                    <span class="carousel-control-prev-icon"></span>
+                                </button>
+                                <button class="carousel-control-next" type="button" data-bs-target="#carouselGallery" data-bs-slide="next">
+                                    <span class="carousel-control-next-icon"></span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- SCRIPT pour activer le bon slide au clic -->
+            <script>
+                document.querySelectorAll('.gallery-thumb').forEach((thumb, index) => {
+                    thumb.addEventListener('click', () => {
+                        const carousel = new bootstrap.Carousel(document.getElementById('carouselGallery'));
+                        carousel.to(index);
+                    });
+                });
+            </script>
         </div>
     </div>
+</div>
